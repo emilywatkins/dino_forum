77f54addbb8d3a688e439297620f373b3c18ab0e
Alex Ficklin and Emily Watkins <emily@>
2018-04-18 16:40:13 -0700

    add styling and seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Topic.destroy_all
+Discussion.destroy_all
+Post.destroy_all
+
+25.times do |i|
+  Topic.create!(title: Faker::Name.name)
+  @topic_id = Topic.last.id
+  5.times do |i|
+    @description = Faker::Hipster.paragraph(10)
+    Discussion.create!(topic_id: @topic_id, description: @description)
+  end
+end
+
+p "Created #{Topic.count} topics"
+p "Created #{Discussion.count} discussions"

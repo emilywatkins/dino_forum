0d29096598696cc77e41c832bb7c5965436ad9c7
Alex Ficklin and Emily Watkins <emily@>
2018-04-18 13:38:09 -0700

    add topic edit functionality and routing.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,20 +3,38 @@ class TopicsController < ApplicationController
     @topics = Topic.all
     render :index
   end
+
   def show
     @topic = Topic.find(params[:id])
     render :show
   end
+
   def new
     @topic = Topic.new
     render :new
   end
+
   def create
     @topic = Topic.new(topic_params)
     if @topic.save
       redirect_to  topics_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update(topic_params)
+      flash[:notice] = "updated topic"
+      redirect_to topics_path
+    else
+      flash[:alert] = "try again error occured"
+      render :edit
     end
   end
 
